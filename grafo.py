fe4989b8e4188e18e1a75d9d358a5c58bb608c0c
Pedronorton <38379173+Pedronorton@users.noreply.github.com>
2019-02-12 17:37:54 -0200

    Update grafo.py

--- grafo.py
+++ grafo.py
@@ -1,7 +1,20 @@
+class Grafo:
+    def __init__(self):
+        self.qtdVertices = None
+        self.qtdArestas = None
+        self.listaArestas = None
+        self.listaVertices = None
+
 class Vertice:
     def __init__(self,id):
         self.id = id
         self.pai = None
-        self.tA = float('inf')
-        self.tF = float('inf')
+        self.abertura = float('inf')
+        self.fechamento = float('inf')
         self.cor = 'B'
+
+class Aresta:
+    def __init__(self, aresta,peso):
+        self.tipo = None
+        self.aresta = aresta
+        self.peso = peso
