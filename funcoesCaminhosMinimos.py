ba92da4c26c9df36b24d1f58d563e80c3478d376
Pedronorton <38379173+Pedronorton@users.noreply.github.com>
2019-02-19 16:40:01 -0300

    Update funcoesCaminhosMinimos.py

--- funcoesCaminhosMinimos.py
+++ funcoesCaminhosMinimos.py
@@ -4,6 +4,7 @@ import math
 
 def floydWarshall(vertices,arestas,G):
     distancias = matrizDistancias(G,arestas,vertices)
+    global menorDistancia
     menorDistancia = distancias
     pais = matrizPais(G,arestas,vertices)
 
@@ -35,27 +36,25 @@ def floydWarshall(vertices,arestas,G):
                     menorDistancia[i.id][j.id] = 0
                     pais[i.id][j.id] = i.id
 
-    print(imprimeTodos(pais,vertices))
+    print(imprimeTodos(pais,menorDistancia,vertices))
     return menorDistancia
 
 
-def imprimeTodos(pais,vertices):
-    for i in vertices:
-        for j in vertices:
+def imprimeTodos(pais,menorDistancia, vertices):
+    #FAZ TODOS OS CAMINHOS DE V->U
+    for v in vertices:
+        for u in vertices:
             global caminho
             caminho = []
-            print('caminho de: ',i.id,' para: ',j.id,' é: ', imprimeCaminho(pais,i.id,j.id))
+            print('Distancia de ',v.id,' até ',u.id,' é: ',menorDistancia[v.id][u.id],' com caminho: ',imprimeCaminho(pais,v.id,u.id))
 
 
 def imprimeCaminho(pais,v,u):#RETORNA O CAMINHO DE V PARA U
-    caminho.append(v)
-    if(pais[v][u] != v):
-        imprimeCaminho(pais,pais[v][u],u)
-    elif(v!=u):
+    if(pais[v][u] != None):#CASO HOUVER UM NONE NA MATRIZ PAI, QUER DIZER QUE NAO POSSUI CAMINHO
         caminho.append(u)
+        if(pais[v][u] != v):
+            imprimeCaminho(pais,v,pais[v][u])
+        elif(v!=u):#CASO V!=U ENTAO BASTA COLOCAR O VERTICE INICIAL E INVERTEER A LISTA
+            caminho.append(v)
+            caminho.reverse()
     return caminho
-
-
-
-
-
